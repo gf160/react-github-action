1aecab76f9caf3b773ab1aba2b09d1c2b21b03da
LeeYouje <gf160@naver.com>
2023-05-23 20:36:49 +0900

    init

--- src/App.js
+++ src/App.js
@@ -1,22 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [disabled, setDisabled] = useState(false);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{count}</h3>
+
+        <button data-testid="minus-btn"
+          onClick={() => setCount(prev => prev === 0 ? 0 : prev - 1)}
+          disabled={disabled}
+          >-</button>
+        <button data-testid="plus-btn" 
+          onClick={() => setCount(prev => prev + 1)}
+          disabled={disabled}
+          >+</button>
+
+        <div>
+          <button data-testid="on/off-btn"
+            style={{backgroundColor: "blue"}}
+            onClick={() => {setDisabled(prev => !prev)}}
+          >on/off</button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,50 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('the counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const counterElement = screen.getByTestId("counter");
+  expect(counterElement).toHaveTextContent("0");
 });
+
+test('minus button has correct text', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId("minus-btn");
+  expect(buttonEl).toHaveTextContent("-");
+});
+
+test('plus button has correct text', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId("plus-btn");
+  expect(buttonEl).toHaveTextContent("+");
+});
+
+test('when the + button is pressed, the counter changes to 1 ', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId('plus-btn');
+  fireEvent.click(buttonEl);
+  const counterEl = screen.getByTestId('counter');
+  expect(counterEl).toHaveTextContent("1");
+
+})
+test('when the - button is pressed, the counter changes to 0 ', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId('minus-btn');
+  fireEvent.click(buttonEl);
+  const counterEl = screen.getByTestId('counter');
+  expect(counterEl).toHaveTextContent("0");
+})
+
+test('on/off button has blue color', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId("on/off-btn");
+  expect(buttonEl).toHaveStyle('background-color:blue');
+})
+
+test('prevent the +,- button from being pressed when the on/off button click', () => {
+  render(<App />);
+  const buttonEl = screen.getByTestId('on/off-btn');
+  fireEvent.click(buttonEl);
+  const plusBtn = screen.getByTestId('plus-btn');
+  expect(plusBtn).toBeDisabled(true);
+})
